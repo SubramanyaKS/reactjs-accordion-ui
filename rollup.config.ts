ad0b6d815c721f64158751bb28d7785f46dc7fce
Subramanya K S <subramanyaks2205@gmail.com>
2024-04-08 06:41:13 +0530

    released v1.0.1

---
+++ rollup.config.ts
@@ -0,0 +1,43 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { dts } from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import terser  from '@rollup/plugin-terser'; // Update import for terser
+
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: packageJson.main,
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        // Set include option to include CSS files in TypeScript bundle
+        include: [/\.tsx?$/, /\.css$/],
+      }),
+      resolve(),
+      commonjs(),
+      postcss({
+        extract: false, // Disable CSS extraction
+        inject: true, // Inject CSS into the bundle
+        minimize: true,
+        // Add any additional postcss plugins if needed
+      }),
+      terser(),
+      
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: 'dist/cjs/types/src/index.d.ts',
+    output: { file: 'dist/index.d.ts', format: 'esm' },
+    plugins: [dts()],
+    external: [/\.css$/],
+  },
+];
